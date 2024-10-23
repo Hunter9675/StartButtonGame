66b7c0daaa401063b11be315a5b6f78da04a3057
huntesta349 <huntesta349@SDS-L-H85HC63>
2024-10-23 11:17:29 -0400

    Add project files.

---
+++ StartButtonGame/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace StartButtonGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.startButton = new System.Windows.Forms.Button();
+            this.threeTwoOneCountdownLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("MV Boli", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(206, 120);
+            this.startButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(498, 267);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start Button";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // threeTwoOneCountdownLabel
+            // 
+            this.threeTwoOneCountdownLabel.AutoSize = true;
+            this.threeTwoOneCountdownLabel.Font = new System.Drawing.Font("MV Boli", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.threeTwoOneCountdownLabel.ForeColor = System.Drawing.Color.White;
+            this.threeTwoOneCountdownLabel.Location = new System.Drawing.Point(350, 157);
+            this.threeTwoOneCountdownLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.threeTwoOneCountdownLabel.Name = "threeTwoOneCountdownLabel";
+            this.threeTwoOneCountdownLabel.Size = new System.Drawing.Size(0, 125);
+            this.threeTwoOneCountdownLabel.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 554);
+            this.Controls.Add(this.threeTwoOneCountdownLabel);
+            this.Controls.Add(this.startButton);
+            this.Font = new System.Drawing.Font("MV Boli", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "The Start Button Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label threeTwoOneCountdownLabel;
+    }
+}
+
---
+++ StartButtonGame/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Media;
+
+namespace StartButtonGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            SoundPlayer sp = new SoundPlayer(Properties.Resources.Censored_Beep_Mastercard_569981218);
+            sp.Play();
+
+            startButton.Visible = false;
+
+            BackColor = Color.Green;
+
+            threeTwoOneCountdownLabel.Text = "3";
+            Refresh();
+            Thread.Sleep(2000);
+
+            threeTwoOneCountdownLabel.Text = "2";
+            sp.Play();
+            Refresh();
+            Thread.Sleep(2000);
+
+            threeTwoOneCountdownLabel.Text = "1";
+            sp.Play();
+            Refresh();
+            Thread.Sleep(2000);
+
+            threeTwoOneCountdownLabel.Text = "GO!!!!";
+            sp = new SoundPlayer(Properties.Resources.Air_Horn_SoundBible_com_964603082);
+            sp.Play();
+
+
+
+        }
+    }
+}
